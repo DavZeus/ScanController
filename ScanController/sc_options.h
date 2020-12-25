8f1dd717d24a7d074c8688a16d2f272c54019012
DavZeus <60774224+DavZeus@users.noreply.github.com>
2020-12-25 04:27:40 +0300

    fix: define return type of function

--- ScanController/sc_options.h
+++ ScanController/sc_options.h
@@ -9,7 +9,7 @@ public:
 	constexpr static char help_switch[] = "help";
 	constexpr static char com_switch[] = "com";
 
-	auto parse(int argc, char* argv[]);
+	auto parse(int argc, char* argv[]) -> void;
 	[[nodiscard]] auto get_value(const std::string& key) const;
 };
 
