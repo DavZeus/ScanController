dd1a82919bfc7b17a3ff52528eb754316b23a945
DavZeus <60774224+DavZeus@users.noreply.github.com>
2020-12-29 21:09:15 +0300

    feat: add class that construct mesh from point cloud

---
+++ ScanController/model_constructor.cpp
@@ -0,0 +1,65 @@
+﻿#include "model_constructor.h"
+
+#ifndef CGAL_LINKED_WITH_TBB
+#define CGAL_LINKED_WITH_TBB
+#endif
+
+#include <CGAL/compute_average_spacing.h>
+#include <CGAL/grid_simplify_point_set.h>
+#include <CGAL/jet_smooth_point_set.h>
+#include <CGAL/remove_outliers.h>
+
+auto model_constructor::remove_outliers_from_set(point_set &points) const
+    -> void {
+  auto outliers_iterator = CGAL::remove_outliers<CGAL::Parallel_tag>(
+      points, 24, points.parameters().threshold_percent(5.0));
+  points.remove(outliers_iterator, points.end());
+  points.collect_garbage();
+}
+
+auto model_constructor::simplify_set(point_set &points) const -> void {
+  const auto spacing =
+      CGAL::compute_average_spacing<CGAL::Parallel_tag>(points, 6);
+  auto simplification_iterator =
+      CGAL::grid_simplify_point_set(points, 2. * spacing);
+  points.remove(simplification_iterator, points.end());
+  std::cout << points.number_of_removed_points()
+            << " point(s) removed after simplification." << std::endl;
+  points.collect_garbage();
+}
+
+auto model_constructor::smooth_set(point_set &points) -> void {
+  CGAL::jet_smooth_point_set<CGAL::Parallel_tag>(points, 24);
+}
+
+auto model_constructor::process_additional(point_set &points) -> void {
+  if (options_ & remove_outliers) {
+    remove_outliers_from_set(points);
+  }
+  if (options_ & simplification) {
+    simplify_set(points);
+  }
+  if (options_ & smooth) {
+    smooth_set(points);
+  }
+}
+
+auto model_constructor::make_mesh(point_set points) -> surface_mesh {
+  process_additional(points);
+  std::function<surface_mesh(point_set)> method_function;
+  switch (method_) {
+  case method::advancing_front:
+    method_function = std::bind_front(&do_advancing_front, this);
+    break;
+  case method::scale_space:
+    method_function = std::bind_front(&do_scale_space, this);
+    break;
+  default:
+    throw std::exception("There is not such model construction method");
+  }
+  return method_function(points);
+}
+
+model_constructor::model_constructor(const method method,
+                                     const additional_options options)
+    : method_(method), options_(options) {}
---
+++ ScanController/model_constructor.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/Surface_mesh.h>
+#include <type_traits>
+
+class model_constructor {
+  using point_set =
+      CGAL::Point_set_3<CGAL::Epick::Point_3, CGAL::Epick::Vector_3>;
+  using surface_mesh = CGAL::Surface_mesh<CGAL::Epick::Point_3>;
+
+public:
+  enum class method : int8_t { advancing_front, scale_space };
+
+private:
+  method method_;
+
+public:
+  // I do not know how to make this enum better because I can not overload
+  // operators of nested enum class
+
+  enum additional_options {
+    remove_outliers = 1,
+    simplification = remove_outliers * 2,
+    smooth = simplification * 2,
+    all = remove_outliers | simplification | smooth
+  };
+
+private:
+  additional_options options_;
+
+  auto remove_outliers_from_set(point_set &points) const -> void;
+  auto simplify_set(point_set &points) const -> void;
+  auto smooth_set(point_set &points) -> void;
+
+  auto process_additional(point_set &points) -> void;
+
+  auto do_advancing_front(point_set points) -> surface_mesh;
+  auto do_scale_space(point_set points) -> surface_mesh;
+
+public:
+  auto make_mesh(point_set points) -> surface_mesh;
+
+  explicit model_constructor(method method, additional_options options = all);
+};
