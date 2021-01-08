22d4535d2620b2e2ac4691f36e8617c3d6730af3
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-01-08 22:42:52 +0300

    fix: add pixel size value to correct convertion formula

--- ScanController/dimension_converter.h
+++ ScanController/dimension_converter.h
@@ -9,16 +9,17 @@ class dimension_converter {
       CGAL::Point_set_3<CGAL::Epick::Point_3, CGAL::Epick::Vector_3>;
   using vertical = std::vector<std::pair<float, float>>;
 
-  constexpr static float min_angle = 1.8F;
-  constexpr static float straight_angle = 180.F;
-  constexpr static float focus_length = 16.F;
-  constexpr static float camera_width_resolution = 3840.F;
+  constexpr static float min_angle = 1.8F; // degrees
+  constexpr static float pixel_size = 1.67; // micrometers
+  constexpr static float straight_angle = 180.F; // degrees
+  constexpr static float focus_length = 16.F; // millimeters
+  constexpr static float camera_width_resolution = 3840.F; // pixels
   constexpr static float camera_height_resolution = 2748.F;
-  constexpr static float half_camera_width = camera_width_resolution / 2.F;
-  constexpr static float half_camera_height = camera_height_resolution / 2.F;
+  constexpr static float half_camera_width = camera_width_resolution / 2.F; // pixels
+  constexpr static float half_camera_height = camera_height_resolution / 2.F; // pixels
 
-  float camera_distance_;
-  float cut_level_;
+  float camera_distance_; // millimeters
+  float cut_level_; // pixels
 
 public:
   explicit dimension_converter(float camera_distance, float cut_level = 0.F);
