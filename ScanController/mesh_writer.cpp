cf4d6046807faae4be11bb52ee23a290b94bb02f
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-01-08 22:47:45 +0300

    fix: add flsuh to save buffer to actual file

--- ScanController/mesh_writer.cpp
+++ ScanController/mesh_writer.cpp
@@ -6,4 +6,6 @@
 auto mesh_writer::write(std::string path, const surface_mesh &mesh) -> void {
   std::ofstream file(path);
   CGAL::write_STL(mesh, file);
+  //Write content to file
+  file.flush();
 }
