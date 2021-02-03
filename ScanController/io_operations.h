df09b2248dce7df2ad02bf096e072fc4b44d2376
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-02-03 13:37:30 +0300

    fix: remove static qualifiers from function declarations

--- ScanController/io_operations.h
+++ ScanController/io_operations.h
@@ -7,12 +7,11 @@ namespace sc {
 namespace io {
 
 // Write mesh to stl file
-static auto write_mesh(std::string path, const surface_mesh &mesh) -> void;
+auto write_mesh(std::string path, const surface_mesh &mesh) -> void;
 // Write data camera point data
-static auto write_data_points(const data_points &points, std::string filename)
-    -> bool;
+auto write_data_points(const data_points &points, std::string filename) -> bool;
 // Read data saved camera point data
-static auto read_data_points(const std::string &filename) -> data_points;
+auto read_data_points(const std::string &filename) -> data_points;
 
 } // namespace io
 
