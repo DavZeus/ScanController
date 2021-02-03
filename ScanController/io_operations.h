54d7745cbcbccd0c777d266c7c2ee9e704919f7a
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-02-04 02:58:35 +0300

    fix: add default empty string parameter

--- ScanController/io_operations.h
+++ ScanController/io_operations.h
@@ -11,7 +11,8 @@ auto generate_time_string() -> std::string;
 // Write mesh to stl file
 auto write_mesh(std::string path, const surface_mesh &mesh) -> void;
 // Write data camera point data
-auto write_data_points(const data_points &points, std::string filename) -> bool;
+auto write_data_points(const data_points &points, std::string filename = {})
+    -> bool;
 // Read data saved camera point data
 auto read_data_points(const std::string &filename) -> data_points;
 
