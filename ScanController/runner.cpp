3a0d705ca03467c0936c49d83b7dd3b52f281898
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-02-04 03:06:31 +0300

    feat: add class that perform program execute sequence

--- ScanController/runner.cpp
+++ ScanController/runner.cpp
@@ -3,36 +3,58 @@
 #include "dimension_converter.h"
 #include "io_operations.h"
 #include "model_constructor.h"
-#include "scan_handler.h"
 
 #include <fmt/ostream.h>
+
+auto sc::runner::do_scan_branch() -> data_points {
+  const auto com =
+      "COM" +
+      std::to_string(options_.get_value(options::com_switch).as<short>());
+  scan_handler scanner(com);
+  return scanner.start();
+}
+
+auto sc::runner::do_data_load_branch() -> data_points {
+  if (options_.check_value(options::load_switch) > 1) {
+    throw std::exception("Only one file can be passed");
+  }
+  const std::string file =
+      options_.get_value(options::load_switch).as<std::string>();
+  return io::read_data_points(file);
+}
 
 auto sc::runner::start(int argc, char *argv[]) -> int {
   try {
     options_.parse(argc, argv);
     if (options_.check_value(options::help_switch)) {
       fmt::print("{}\n", options_.get_allowed_options());
-      return 0;
+      return EXIT_SUCCESS;
     }
     if (!options_.check_value(options::distance_switch)) {
       throw std::exception("Distance to camera is not specified");
     }
-    const auto dist = options_.get_value(options::distance_switch).as<float>();
-    const auto com =
-        "COM" +
-        std::to_string(options_.get_value(options::com_switch).as<short>());
-    scan_handler scanner(com);
-    auto points = scanner.start();
-    const auto cut = options_.get_value(options::cut_switch).as<float>();
-    const dimension_converter converter(dist, cut);
-    auto p = converter.convert(std::move(points));
+    data_points d_points;
+    if (options_.check_value(options::load_switch)) {
+      d_points = do_data_load_branch();
+    } else {
+      d_points = do_scan_branch();
+    }
+    if (options_.check_value(options::save_switch) &&
+        !options_.check_value(options::load_switch)) {
+      io::write_data_points(d_points);
+    }
+    const auto cut_level = options_.get_value(options::cut_switch).as<float>();
+    const auto camera_distance =
+        options_.get_value(options::distance_switch).as<float>();
+    const dimension_converter converter(camera_distance, cut_level);
+    const auto points = converter.convert(std::move(d_points));
     const model_constructor constructor(
         model_constructor::methods::advancing_front);
-    auto mesh = constructor.make_mesh(p);
-    io_operations::write_mesh("model.stl", mesh);
+    const auto mesh = constructor.make_mesh(points);
+    io::write_mesh("model.stl", mesh);
   } catch (const std::exception &ex) {
     fmt::print(stderr, "\n{}\n", ex.what());
-    return 1;
+    return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
--- ScanController/runner.h
+++ ScanController/runner.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "options.h"
+#include "scan_handler.h"
 
 namespace sc {
 class runner {
   options options_{};
+
+  auto do_scan_branch() -> data_points;
+  auto do_data_load_branch() -> data_points;
 
 public:
   auto start(int argc, char *argv[]) -> int;
