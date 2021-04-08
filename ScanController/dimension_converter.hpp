b77596b14325d559eee5af9fc49b3cbaa7a04cfe
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-04-08 15:43:54 +0300

    feat: realize new conversion formula

--- ScanController/dimension_converter.hpp
+++ ScanController/dimension_converter.hpp
@@ -9,20 +9,21 @@ template <std::floating_point T> class dimension_converter {
   // Basler ace GigE camera acA1440-73gm
   // Basler Lens C125-0418-5M-P
   inline static constexpr T pixel_size =
-      static_cast<T>(3.45); // micrometers, camera
-  inline static constexpr T full_angle = static_cast<T>(360.F);     // degrees
-  inline static constexpr T straight_angle = static_cast<T>(180.F); // degrees
-  inline static constexpr T focus_length = static_cast<T>(4.09F);
+      static_cast<T>(3.45) /
+      static_cast<T>(1000); // millimeters, original - micrometers, camera
+  inline static constexpr T full_angle = static_cast<T>(360);     // degrees
+  inline static constexpr T straight_angle = static_cast<T>(180); // degrees
+  inline static constexpr T focus_length = static_cast<T>(4.09);
   // millimeters, lens
-  inline static constexpr T camera_width_resolution = static_cast<T>(1088.F);
+  inline static constexpr T camera_width_resolution = static_cast<T>(1088);
   // pixels, camera
-  inline static constexpr T camera_height_resolution = static_cast<T>(1456.F);
+  inline static constexpr T camera_height_resolution = static_cast<T>(1456);
   // pixels, camera
   inline static constexpr T half_camera_width =
-      camera_width_resolution / static_cast<T>(2.F);
+      camera_width_resolution / static_cast<T>(2);
   // pixels
   inline static constexpr T half_camera_height =
-      camera_height_resolution / static_cast<T>(2.F);
+      camera_height_resolution / static_cast<T>(2);
   // pixels
 
   // Distance from camera to axis of rotation (millimeters)
@@ -43,14 +44,12 @@ template <std::floating_point T>
 auto dimension_converter<T>::convert(model_profiles<T> &&d_points) const
     -> point_set {
   const T angle = full_angle / static_cast<T>(d_points.size());
-  const T cathetus =
-      static_cast<T>(camera_distance_) / std::numbers::sqrt2_v<T>;
 
-  constexpr T micro_multiplier = static_cast<T>(1000.F);
+  const T d = camera_distance_ - focus_length;
 
   point_set cloud;
   unsigned angle_multiplier = 0;
-  T rad = static_cast<T>(0.F);
+  T rad = static_cast<T>(0);
 
   for (const auto &profile_pair : d_points) {
     const T cos_rad = std::cos(rad);
@@ -58,18 +57,14 @@ auto dimension_converter<T>::convert(model_profiles<T> &&d_points) const
     for (const auto &profile : profile_pair) {
       if (!profile.empty()) {
         for (const auto &point : profile) {
-          const T hx = (point.x - half_camera_width) *
-                       pixel_size / micro_multiplier;
-          const T p_x = camera_distance_ +
-                        (camera_distance_ - focus_length) * hx / focus_length;
-          const T hz =
-              (point.z - half_camera_height) * pixel_size / micro_multiplier;
-          const T z = (camera_distance_ - focus_length) * hz / focus_length;
 
-          const T cos_a = cathetus / p_x;
-          const T sin_a = std::sqrt(static_cast<T>(1.F) - cos_a * cos_a);
-          const T opposing = p_x * sin_a;
-          const T r = cathetus - opposing;
+          const T hx = (point.x - half_camera_width) * pixel_size;
+          const T mx = hx * d;
+          const T r = mx / (mx + focus_length);
+
+          const T hz = (point.z - half_camera_height) * pixel_size;
+          const T mz = hz * d;
+          const T z = hz / (hz + focus_length);
 
           const T x = cos_rad * r;
           const T y = sin_rad * r;
